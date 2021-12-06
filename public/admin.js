cb0d8015b29eb3801c7a356e3a827603c49aa487
Andrew Tapalla <89113477+andrewtapalla@users.noreply.github.com>
2021-12-06 12:44:05 -0800

    Add files via upload

---
+++ public/admin.js
@@ -0,0 +1,33 @@
+async function main() {
+    let response = await fetch('http://localhost:3001/listBooks');
+    let books = await response.json();
+    books.forEach(renderBook);
+}
+
+function renderBook(book) {
+    let room = document.querySelector('#root');
+
+    let li = document.createElement('li');
+    li.textContent = book.title;
+
+    let quantityInput = document.createElement('input');
+    quantityInput.value = book.quantity;
+
+    let submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+
+    submitButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+}
+
+main()
